1e1f7fb79aa0aae2796ac6ae9f609107f4a07d27
54185-Nutsuda <54185@yupparaj.ac.th>
2026-09-09 16:23:28 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -45,8 +45,7 @@ def show_result_dialog(ans1, ans2):
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
- st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
-
+    st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
     if score == 2:
         st.success("🎉 You win!")
     else:
