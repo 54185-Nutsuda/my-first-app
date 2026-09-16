207ede3d3ea0edd2aa3afc68a0c1917add1d7e39
54185-Nutsuda <54185@yupparaj.ac.th>
2026-09-16 16:17:01 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -85,6 +85,7 @@ ans3 = st.text_input(
 ans4 = st.text_input(
     "ข้อ 1: I have `P _ n` . 🖊️",
     value=st.session_state.ans1_val,
+)
 
 # อัปเดตค่าล่าสุดเข้าตัวแปร
 st.session_state.ans1_val = ans1
