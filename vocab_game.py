da4ef231386d8a5ee835957dd30a5ae9f85f142b
54185-Nutsuda <54185@yupparaj.ac.th>
2026-09-21 21:37:13 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -125,7 +125,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2, ans3, ans4)
+    show_result_dialog(ans1, ans2)
 
 st.divider()
 st.write("นางสาวณัฐสุดา ฟองคำ เลขที่ 3 ม.4/4")
